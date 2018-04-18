231173fc6c7e702b230430dfe2753b37c01d0928
Cristian Davila <krif07@gmail.com>
2018-04-17 21:34:25 -0500

    Se cambia la consulta del detalle de la factura para hacer una sólo con join y no varias por cada tabla

--- src/main/java/com/co/cbg/sbt/app/models/dao/IFacturaDao.java
+++ src/main/java/com/co/cbg/sbt/app/models/dao/IFacturaDao.java
@@ -1,9 +1,12 @@
 package com.co.cbg.sbt.app.models.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.co.cbg.sbt.app.models.entity.Factura;
 
 public interface IFacturaDao extends CrudRepository<Factura, Long>{
 
+	@Query("SELECT f FROM Factura f JOIN FETCH f.cliente c JOIN FETCH f.items l JOIN FETCH l.producto p WHERE f.id=?1")
+	public Factura fetchByIdWithClienteWithItemFacturaWithProducto(Long id);
 }
--- src/main/java/com/co/cbg/sbt/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/co/cbg/sbt/app/models/service/ClienteServiceImpl.java
@@ -87,5 +87,11 @@ public class ClienteServiceImpl implements IClienteService{
 	public void deleteFactura(Long id) {
 		facturaDao.delete(id);
 	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Factura fetchFacturaByIdWithClienteWithItemFacturaWithProducto(Long id) {		
+		return facturaDao.fetchByIdWithClienteWithItemFacturaWithProducto(id);
+	}
 	
 }
