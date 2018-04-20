1def1743f1f81919def190bbefd7210c1ce7ebbd
Cristian Davila <krif07@gmail.com>
2018-04-19 22:46:25 -0500

    Se adiciona funcionalidad para cerrar sesión y para mostrar links y forms para iniciar y cerrar sesión

--- src/main/java/com/co/cbg/sbt/app/controllers/LoginController.java
+++ src/main/java/com/co/cbg/sbt/app/controllers/LoginController.java
@@ -12,7 +12,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(@RequestParam(value="error", required=false) String error, 
+	public String login(@RequestParam(value="error", required=false) String error,
+			@RequestParam(value="logout", required=false) String logout,
 			Model model, Principal principal, RedirectAttributes flash) {
 		
 		if(principal != null) {			
@@ -22,6 +23,10 @@ public class LoginController {
 		
 		if(error != null) {
 			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrectos");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("success", "Ha cerrado sesión con éxito");
 		}
 		
 		return "login";
