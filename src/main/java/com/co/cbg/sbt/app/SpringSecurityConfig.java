0bd4d8a9c3aaedb88ebbb13e247220cea87f9b4d
Cristian Davila <krif07@gmail.com>
2018-06-24 00:02:51 -0500

    se cambia la consulta de sql a jpa en la clase springSecurityConfig

--- src/main/java/com/co/cbg/sbt/app/SpringSecurityConfig.java
+++ src/main/java/com/co/cbg/sbt/app/SpringSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.co.cbg.sbt.app;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +13,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.co.cbg.sbt.app.auth.handler.LoginSuccessHandler;
+import com.co.cbg.sbt.app.models.service.JpaUserDetailsService;
 
 @EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 @Configuration
@@ -24,10 +23,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	private LoginSuccessHandler successHandler;
 	
 	@Autowired
-	private DataSource dataSource;
-	
+	private JpaUserDetailsService userDetailsService;
+
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -53,17 +54,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
 		
-		build.jdbcAuthentication()
-		.dataSource(dataSource)
-		.passwordEncoder(passwordEncoder)
-		.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
-		.authoritiesByUsernameQuery("SELECT u.username, a.authority FROM authorities a INNER JOIN users u ON a.user_id = u.id WHERE u.username=?");
+		build.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder);
 		
-		/*PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
-		build.inMemoryAuthentication()
-		.withUser(users.username("admin").password("backtira1").roles("ADMIN", "USER"))
-		.withUser(users.username("krif07").password("backtira1").roles("USER"));*/
 	}
 }
