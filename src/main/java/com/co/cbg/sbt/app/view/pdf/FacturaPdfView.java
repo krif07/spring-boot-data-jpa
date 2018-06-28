b05e3c22f41316303485a4f6dc099ea6675686e2
Cristian Davila <krif07@gmail.com>
2018-06-28 00:12:25 -0500

    Se completa la exportación a pdf de la factura y se le da algo de diseño

--- src/main/java/com/co/cbg/sbt/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/co/cbg/sbt/app/view/pdf/FacturaPdfView.java
@@ -1,5 +1,6 @@
 package com.co.cbg.sbt.app.view.pdf;
 
+import java.awt.Color;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,8 +10,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.co.cbg.sbt.app.models.entity.Factura;
+import com.co.cbg.sbt.app.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;;
 
 @Component("factura/ver")
@@ -24,19 +28,57 @@ public class FacturaPdfView extends AbstractPdfView{
 		
 		PdfPTable tabla = new PdfPTable(1);
 		tabla.setSpacingAfter(20);
-		tabla.addCell("Datos del cliente");
+		
+		PdfPCell cell = null;
+		
+		cell = new PdfPCell(new Phrase("Datos del cliente"));
+		cell.setBackgroundColor(new Color(184, 218, 255));
+		cell.setPadding(8f);
+		
+		tabla.addCell(cell);		
 		tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
 		tabla.addCell(factura.getCliente().getEmail());
 		
 		PdfPTable tabla2 = new PdfPTable(1);
-		tabla2.setSpacingAfter(20);
-		tabla2.addCell("Datos de la factura");
+		tabla2.setSpacingAfter(20);		
+		
+		cell = new PdfPCell(new Phrase("Datos de la factura"));
+		cell.setBackgroundColor(new Color(195, 230, 203));
+		cell.setPadding(8f);
+		
+		tabla2.addCell(cell);		
 		tabla2.addCell("Folio: " + factura.getId());
 		tabla2.addCell("Descripción: " + factura.getDescripcion());
 		tabla2.addCell("Fecha: " + factura.getCreateAt());
 		
+		PdfPTable tabla3 = new PdfPTable(4);
+		tabla3.setWidths(new float[] {3.5f, 1, 1, 1});
+		tabla3.addCell("Producto");
+		tabla3.addCell("Precio");
+		tabla3.addCell("Cantidad");
+		tabla3.addCell("Total");
+		
+		for(ItemFactura item: factura.getItems()) {
+			tabla3.addCell(item.getProducto().getNombre());
+			tabla3.addCell(item.getProducto().getPrecio().toString());
+			
+			cell = new PdfPCell(new Phrase(item.getCantidad().toString()));
+			cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			
+			tabla3.addCell(cell);
+			
+			tabla3.addCell(item.calcularImporte().toString());			
+		}
+		
+		cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+		tabla3.addCell(cell);
+		tabla3.addCell(factura.getTotal().toString());
+		
 		document.add(tabla);
 		document.add(tabla2);
+		document.add(tabla3);
 	}
 
 }
