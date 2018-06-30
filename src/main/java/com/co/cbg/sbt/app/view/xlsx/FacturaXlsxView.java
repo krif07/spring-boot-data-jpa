66f036e0ca100c7158d077f7bf9b980cc5685b70
Cristian Davila <krif07@gmail.com>
2018-06-30 11:50:16 -0500

    Se configura la app para exportar a excel la factura y se crea la primera versión del documento excel de factura a exportar

---
+++ src/main/java/com/co/cbg/sbt/app/view/xlsx/FacturaXlsxView.java
@@ -0,0 +1,69 @@
+package com.co.cbg.sbt.app.view.xlsx;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.co.cbg.sbt.app.models.entity.Factura;
+import com.co.cbg.sbt.app.models.entity.ItemFactura;
+
+@Component("factura/ver.xlsx")
+public class FacturaXlsxView extends AbstractXlsxView{
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		Factura factura = (Factura) model.get("factura");
+		
+		Sheet sheet = workbook.createSheet("Factura " + factura.getCliente().getNombre());
+		
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);
+		
+		cell.setCellValue("Datos del cliente");
+		
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
+		
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getEmail());
+		
+		
+		//Otra forma
+		sheet.createRow(4).createCell(0).setCellValue("Datos de la factura");
+		sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+		sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
+		sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
+		
+		Row header = sheet.createRow(9);
+		header.createCell(0).setCellValue("Producto");
+		header.createCell(1).setCellValue("Precio");
+		header.createCell(2).setCellValue("Cantidad");
+		header.createCell(3).setCellValue("Total");
+		
+		int rowNum = 10;
+		for(ItemFactura item: factura.getItems()) {
+			Row fila = sheet.createRow(rowNum++);
+			fila.createCell(0).setCellValue(item.getProducto().getNombre());
+			fila.createCell(1).setCellValue(item.getProducto().getPrecio());
+			fila.createCell(2).setCellValue(item.getCantidad());
+			fila.createCell(3).setCellValue(item.calcularImporte());
+		}
+		
+		Row filaTotal = sheet.createRow(rowNum);
+		filaTotal.createCell(2).setCellValue("Gran Total");
+		filaTotal.createCell(3).setCellValue(factura.getTotal());
+	}
+
+}
