7a2b2d86650d562fc7202991318b5d6e73003211
Cristian Davila <krif07@gmail.com>
2018-07-13 09:44:41 -0500

    Se sobreescribe la función  successfulAuthentication, que se ejecuta cuando la autenticación fue correcta, aquí se construye la respuesta con el token JWT para el usuario

--- src/main/java/com/co/cbg/sbt/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/co/cbg/sbt/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,5 +1,11 @@
 package com.co.cbg.sbt.app.auth.filter;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,8 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -44,5 +56,31 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		
 		return authenticationManager.authenticate(authToken);
 	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		
+		String token = Jwts.builder()
+				.setSubject(username)
+				.signWith(SignatureAlgorithm.HS512, "mi.clave.secreta.123".getBytes())
+				.compact();
+		
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("mensaje", String.format("Hola %s, Ha iniciado sesión con éxito", username));
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
+				
+	}
+	
+	
 	
 }
